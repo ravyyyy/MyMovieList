d6d6c1013a6d550c6320df22886c4899a52bae49
Efrim Dragos Alexandru <efrimdragos@gmail.com>
2023-06-28 15:31:56 +0300

    Lazy loading
    Lazy loading complete for main module.

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
