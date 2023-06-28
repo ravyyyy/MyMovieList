96ee5aca64fa4b380d10a309a89e5337b01470bb
Efrim Dragos Alexandru <efrimdragos@gmail.com>
2023-06-28 18:24:18 +0300

    Register complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './main/components/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
---
+++ src/app/main/interfaces/register-form-data.ts
@@ -0,0 +1,8 @@
+export interface RegisterFormData {
+    email: string;
+    password: string;
+    confirmPassword: string;
+    firstName: string;
+    lastName: string;
+  }
+  
